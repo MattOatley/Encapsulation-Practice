a85fd56e74a7e6d5bb1bd8538fe35c310e1401b6
Matt Oatley <Matthew.Oatley225@gmail.com>
2020-07-01 10:29:50 -0500

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+   public class BankAccount
+    {
+        private double _balance;
+
+        public void Deposit(double amount) 
+        {
+
+            Console.WriteLine($"Depositing {amount, 0:C} into your account.");
+            _balance = amount;
+
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+        
+        
+        
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("Please enter the amount of $ you would like to deposit");
+            double depositAmount = double.Parse(Console.ReadLine());
+
+            account.Deposit(depositAmount);
+            double accountBalance = account.GetBalance();
+
+            Console.WriteLine($"Your balance is now {accountBalance, 0:C}");
+
+        }
+    }
+   
+}
